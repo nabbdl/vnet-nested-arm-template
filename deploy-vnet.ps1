94950ef1af16d22c47ec3cccd783e1cf527ed888
ABCHICHE Nordine <nab@bourse.lu>
2019-02-20 12:32:25 +0100

    OK

---
+++ deploy-vnet.ps1
@@ -0,0 +1,8 @@
+﻿$location = 'westeurope'
+$rgname = "testnab-rg"
+$templateuri = "https://raw.githubusercontent.com/nabbdl/vnet-nested-arm-template/master/azuredeploy.json"
+New-AzResourceGroup -Name $rgname -Location $location 
+#If it's first deployment use 
+New-AzResourceGroupDeployment -Verbose -ResourceGroupName $rgname -DeploymentDebugLogLevel All -TemplateUri $templateuri -rebuildVNET "Yes"
+#If you just want to update the deployment without deleteing existing resource run without "rebuildVNET" parameters (value set "No" by default)
+New-AzResourceGroupDeployment -Verbose -ResourceGroupName $rgname -DeploymentDebugLogLevel All -TemplateUri $templateuri
